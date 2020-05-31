ea69e87f9304fb3dec6e73aa4a67c4f7fd9f3a48
Taras <taras19041@gmail.com>
2020-05-31 16:03:11 +0300

    Add security to pom.xml add security entities and repos

---
+++ src/main/java/com/kovaliv/lab3/security/entities/User.java
@@ -0,0 +1,31 @@
+package com.kovaliv.lab3.security.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    @Transient
+    private String passwordConfirm;
+
+    @ManyToMany
+    private Set<Role> roles;
+}
---
+++ src/main/java/com/kovaliv/lab3/security/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.kovaliv.lab3.security.repository;
+
+import com.kovaliv.lab3.security.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
