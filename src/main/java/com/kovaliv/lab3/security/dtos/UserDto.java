4ba25f00ac9be6d9572a5556c3032c02349c0b8a
Taras <taras19041@gmail.com>
2020-06-01 17:13:56 +0300

    Edit UserDto

--- src/main/java/com/kovaliv/lab3/security/dtos/UserDto.java
+++ src/main/java/com/kovaliv/lab3/security/dtos/UserDto.java
@@ -1,12 +1,12 @@
 package com.kovaliv.lab3.security.dtos;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDto {
     private Long id;
 
