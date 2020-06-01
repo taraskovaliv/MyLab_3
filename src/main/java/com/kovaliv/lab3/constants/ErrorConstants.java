77b4ce3b80b9f1b2247c1b350bf5155bf8c8c584
Taras <taras19041@gmail.com>
2020-06-01 06:41:50 +0300

    Refactor security

--- src/main/java/com/kovaliv/lab3/constants/ErrorConstants.java
+++ src/main/java/com/kovaliv/lab3/constants/ErrorConstants.java
@@ -2,4 +2,5 @@ package com.kovaliv.lab3.constants;
 
 public class ErrorConstants {
     public static final String USER_NOT_FOUND_BY_USERNAME = "User not found by username: ";
+    public static final String USERNAME_IS_ALREADY_USED = "Username is already used";
 }
--- src/main/java/com/kovaliv/lab3/security/services/UserService.java
+++ src/main/java/com/kovaliv/lab3/security/services/UserService.java
@@ -2,10 +2,14 @@ package com.kovaliv.lab3.security.services;
 
 
 import com.kovaliv.lab3.security.dtos.AddUserRequestDto;
+import com.kovaliv.lab3.security.dtos.LoginDto;
+import com.kovaliv.lab3.security.dtos.UserDto;
 import com.kovaliv.lab3.security.entities.User;
 
 public interface UserService {
-    User save(AddUserRequestDto addUserRequestDto);
+    UserDto save(AddUserRequestDto addUserRequestDto);
 
     User findByUsername(String username);
+
+    UserDto authenticateUser(LoginDto loginDto);
 }
